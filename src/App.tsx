f9bb9fcf48bc83b3a67e303f7ac4124ef30a2967
MichalSajdik <sajdikmichal@gmail.com>
2023-01-22 14:02:25 +0100

    Format code

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,27 @@ import Search from "./components/Search";
 import ListOfCurrencies from "./components/ListOfCurrencies";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Search/>
-      <ListOfCurrencies/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Search/>
+            <ListOfCurrencies/>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
