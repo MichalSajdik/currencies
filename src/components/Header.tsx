49b19c9663e548dbf2058fe93fdbd15d7093eb44
MichalSajdik <sajdikmichal@gmail.com>
2023-01-22 12:01:55 +0100

    add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+  background-color: black;
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+export default function Header(){
+
+    return <HeaderWrapper>
+        George FE Test
+    </HeaderWrapper>
+}
